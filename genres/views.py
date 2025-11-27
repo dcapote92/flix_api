e04879c085306c8f34bae610abd0f8be5a0677a7
dcapote92 <danicapote92@gmail.com>
2025-11-27 12:07:01 -0300

    endpoint to individual genre and treatment for (404) not found error

--- genres/views.py
+++ genres/views.py
@@ -1,10 +1,11 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from genres.models import Genre
 
 @csrf_exempt
-def genre_view(request) -> JsonResponse:
+def genre_create_list_view(request) -> JsonResponse:
     if request.method == 'GET':
         genres = Genre.objects.all()
         data = [ { 'id':genre.id, 'name': genre.name} for genre in genres ]
@@ -18,3 +19,9 @@ def genre_view(request) -> JsonResponse:
             {'id': new_genre.id, 'name': new_genre.name},
             status=201,
         )
+    
+@csrf_exempt
+def genre_detail_view(request, pk) -> JsonResponse:
+    genre = get_object_or_404(Genre, pk=pk)
+    data = ({ 'id': genre.id, 'name': genre.name})
+    return JsonResponse (data) 
