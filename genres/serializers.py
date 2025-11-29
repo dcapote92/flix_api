36918a32a19e61e1abd65028a192deb11e6e5ef1
dcapote92 <danicapote92@gmail.com>
2025-11-29 12:42:00 -0300

    get_all and post changed to rest_framework

---
+++ genres/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from genres.models import Genre
+
+class GenreSerializer(serializers.ModelSerializer):
+    
+    class Meta(Genre):
+        model = Genre
+        fields = '__all__' #['id', 'name']
