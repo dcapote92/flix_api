36c7c5de0d1e428168aa7676e9bf54d9f7a402ba
dcapote92 <danicapote92@gmail.com>
2025-11-29 17:08:00 -0300

    app actors created and configured

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 
 
 urlpatterns = [
@@ -8,4 +9,7 @@ urlpatterns = [
 
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
     path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+
+    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
 ]
