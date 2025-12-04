b8f6257e8e34c3a56f5d183186e9f3b64af6be89
dcapote92 <danicapote92@gmail.com>
2025-12-04 11:51:07 -0300

    getting movie rate using django-orm

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
@@ -9,18 +10,24 @@ class MovieModelSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_rate(self, obj):
-        reviews = obj.reviews.all()
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
 
-        if reviews:
-            sum_reviews = 0
-            reviews_count = reviews.count()
-        
-            for review in reviews:
-                sum_reviews+= review.stars
-        
-            return round(sum_reviews/ reviews_count, 1)
+        if rate:
+            return rate
         
         return None
+        # reviews = obj.reviews.all()
+
+        # if reviews:
+        #     sum_reviews = 0
+        #     reviews_count = reviews.count()
+        
+        #     for review in reviews:
+        #         sum_reviews+= review.stars
+        
+        #     return round(sum_reviews/ reviews_count, 1)
+        
+        # return None
 
     def validate_release_date(self, value):
         if value.year < 1900:
