86245b5d4d662df08e7165e9bcd57ecda7d9dd87
dcapote92 <danicapote92@gmail.com>
2025-12-05 07:48:11 -0300

    setting djangorestframework_simplejwt as default auth method

--- app/settings.py
+++ app/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     
     'django_extensions',
     'rest_framework',
+    'rest_framework_simplejwt',
     
+    'authentication',
     'genres',
     'actors',
     'movies',
@@ -128,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
