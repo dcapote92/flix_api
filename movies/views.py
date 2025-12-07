9693c80b903579a137836033576104256864c8a5
dcapote92 <danicapote92@gmail.com>
2025-12-07 08:45:57 -0300

    custom endpoint to get movies stats

--- movies/views.py
+++ movies/views.py
@@ -1,16 +1,42 @@
-from rest_framework import generics
+from django.db.models import Count, Avg
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 from movies.models import Movie
 from movies.serializers import MovieModelSerializer
-from app.permissions import GlobalDefaultPermission
+from reviews.models import Review
 
 class MovieCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
+
+# Custom View to return custom data
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.all().count()
+        avg_stars =Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
+        data = {
+            'total_movies': total_movies,
+            'movies_by_genre': movies_by_genre,
+            'total_reviews': total_reviews,
+            'average_stars': round(avg_stars, 1) if avg_stars else 0
+        }
+
+        
+
+        return response.Response(data, status=status.HTTP_200_OK)
+
+    
 
